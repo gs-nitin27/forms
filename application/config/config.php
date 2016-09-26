0065595a3e6ca19a68f4b3047dd9f525280fa0e0
nitin agarwal <nitin@darkhorsesports.in>
2016-09-26 10:04:41 +0530

    uploading chencges

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/forms/';
+$config['base_url'] = 'http://localhost/forms';
 $config['site_url'] = 'http://localhost/forms/index.php/';
 
 /*
