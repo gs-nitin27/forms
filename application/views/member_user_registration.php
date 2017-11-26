db0990368d01eb2face4cebdabbf41f061d62f35
nitn <nitin@darkhorsesports.in>
2017-11-27 01:37:50 +0530

    bug fixing

--- application/views/member_user_registration.php
+++ application/views/member_user_registration.php
@@ -490,11 +490,11 @@ if(professions!=null)
                 text: 'Thank You !',
                 btnClass: 'btn-green',
                 action: function(){
-                   window.location.href ="<?php echo site_url('forms/editRegisterUserProfile');?>";
+                  // window.location.href = "https://getsporty.in/web/#/login";
                 }
             },
             close: function () {
-               window.location.href = "<?php echo site_url('forms/editRegisterUserProfile');?>";
+              // window.location.href = "https://getsporty.in/web/#/login";
             }
         }
     });
