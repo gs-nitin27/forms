f1f956bd820ab387a41c10330d45060b9237f93b
nitn <nitin@darkhorsesports.in>
2017-11-26 16:14:23 +0530

    no profile image case

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title></title>
+<title>Getsporty::Profile</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo base_url('assets/css/profile.css'); ?>">
